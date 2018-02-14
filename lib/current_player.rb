907b32d6a2c7526a7ef77a0ebeee49635b1a7516
Denise Pendleton <dpendl1@hotmail.com>
2018-02-14 04:11:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,11 +6,16 @@ def turn_count(board)
   end
 end
 
-# def current_player(board)
-#   turn_count(board)
+def current_player(board)
+if turn_count(board) % 2 ==0
+  "X"
+else 
+  "O"
+  end
+#   
 #   if turns % 2 == 0
 #     current_player = "X"
 #   else
 #     current_player = "O"
-#   end
+   
 # end
